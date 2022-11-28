eb36628e4fe9084d941e3e03d55127ec3685e42a
vbouche <vincent.boucher02@edu.univ-fcomte.fr>
2022-11-28 11:46:03 +0100

    update manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -32,7 +32,16 @@ self.addEventListener('install', (e) => {
       const response = await fetch(e.request);
       const cache = await caches.open(cacheName);
       console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
+      cache.add(e.request, response.clone());
       return response;
     })());
+  });
+
+  self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key === cacheName) { return; }
+        return caches.delete(key);
+      }));
+    }));
   });
